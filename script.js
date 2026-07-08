0dc66401fe0b48b8cece1f0a3ae4ff4016d37455
fzyva0406-sudo <fzyva0406@gmail.com>
2026-07-09 03:37:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,16 @@ setInterval(() => {
     heart.remove();
   }, 8000);
 }, 500);
+// Music Player
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
+
+musicBtn.addEventListener("click", () => {
+  if (music.paused) {
+    music.play();
+    musicBtn.innerHTML = "⏸ Pause Music";
+  } else {
+    music.pause();
+    musicBtn.innerHTML = "🎵 Play Music";
+  }
+});
